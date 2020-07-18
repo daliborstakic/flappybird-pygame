95f652bba2cfa8b6fd34bcad4297c4cd02a62b04
daliborstakic <stakicdalibor64@gmail.com>
2020-07-18 12:18:45 +0200

    Set up the main game loop aswell as some variables and functions.

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 import pygame
+
+pygame.init()
+
+# Constant variables
+WIDTH = 300
+HEIGHT = 600
+
+# Setting up the screen
+surf = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Game clock
+clock = pygame.time.Clock()
+FPS = 60
+
+
+# Function dedicated to rendering and drawing objects
+def render():
+    pygame.display.update()
+
+
+# Game loop
+def main():
+    while True:
+        clock.tick(FPS)
+
+        for event in pygame.event:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        render()
+
+
+main()
