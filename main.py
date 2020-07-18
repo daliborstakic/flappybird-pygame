2c288b736638fdfb9b11c3f943fe0790bd43d45c
daliborstakic <stakicdalibor64@gmail.com>
2020-07-18 14:45:56 +0200

    Incorporated falling and jumping mechanics.

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import pygame
 
 pygame.init()
 
+
+# Bird class
+class Bird:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    falling_speed = 3
+    jump_force = 50
+
+
 # Constant variables
 WIDTH = 300
 HEIGHT = 500
@@ -26,18 +37,29 @@ BIRD_IMAGE = pygame.transform.scale2x(pygame.image.load("Sprites/bird.png"))
 def render():
     surf.fill(BLACK)
     surf.blit(BACKGROUND, (0, 0))
-    surf.blit(BIRD_IMAGE, (20, (HEIGHT - BIRD_IMAGE.get_height()) / 2))
+    surf.blit(BIRD_IMAGE, (bird.x, bird.y))
     pygame.display.update()
 
 
 # Game loop
 def main():
+    global bird
+
+    bird = Bird(20, (HEIGHT - BIRD_IMAGE.get_height()) // 2)
+
     while True:
         clock.tick(FPS)
+
+        bird.y += bird.falling_speed
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                bird.y -= bird.jump_force
+
+        bird.y = bird.y if bird.y >= 0 else 0
 
         render()
 
