90c45ab60746d3dc2c8baf61cd18dd8a694f1564
daliborstakic <stakicdalibor64@gmail.com>
2020-07-19 20:25:48 +0200

    Added the score.

--- main.py
+++ main.py
@@ -54,30 +54,29 @@ POLE_IMAGE_ROTATED = pygame.image.load("Sprites/pole_rotated.png")
 pygame.display.set_caption("Flappy Bird")
 pygame.display.set_icon(BIRD_IMAGE)
 
+# Fonts
+SCORE_FONT = pygame.font.SysFont('arial', 20)
+
 
 # Function dedicated to rendering and drawing objects
-def render(pole):
+def render(pole, score_text):
     surf.fill(BLACK)
     surf.blit(BACKGROUND, (0, 0))
     surf.blit(POLE_IMAGE, (pole.x, pole.y1 - pole.size_between))
     surf.blit(POLE_IMAGE_ROTATED, (pole.x, pole.y2 + pole.size_between))
     surf.blit(GROUND, (0, HEIGHT - GROUND.get_height()))
     surf.blit(BIRD_IMAGE, (bird.x, bird.y))
+    surf.blit(score_text, (10, 10))
     pygame.display.update()
 
 
 def restart_game():
-    global poles, bird
+    global poles, bird, score
+    score = 0
     poles = []
     poles = [Pole(WIDTH + POLE_IMAGE.get_width(), 0, HEIGHT / 2)]
     bird = Bird(20, (HEIGHT - GROUND.get_height() - BIRD_IMAGE.get_height()) // 2)
-
-
-# Universal function for displaying text
-def display_text(content, color, x, y, size, font):
-    TITLE_TEXT = pygame.font.SysFont(font, size)
-    TITLE_TEXT.render(content, 1, color)
-    surf.blit(TITLE_TEXT, (x, y))
+    pygame.time.delay(1000)
 
 
 # Displays a message using the Tkinter module
@@ -87,25 +86,36 @@ def message_box(subject, content):
     root.withdraw()
     messagebox.showinfo(subject, content)
 
+    try:
+        root.destroy()
+    except:
+        pass
+
 
 # Handling the collision
 def has_collided(pole):
     if (pole.x <= bird.x <= pole.x + POLE_IMAGE.get_width()) and (
-            bird.y <= pole.y1 - pole.size_between + POLE_IMAGE.get_height() or bird.y >= pole.y2):
+            bird.y <= pole.y1 - pole.size_between + POLE_IMAGE.get_height() or bird.y >= pole.y2 + pole.size_between):
         return True
     return False
 
 
 # Game loop
 def main():
-    global bird
+    global bird, poles, score
 
     poles = [Pole(WIDTH + POLE_IMAGE.get_width(), 0, HEIGHT / 2)]
+
+    # Score parameters
+    score = 0
+    score_update = True
 
     bird = Bird(20, (HEIGHT - GROUND.get_height() - BIRD_IMAGE.get_height()) // 2)
 
     while True:
         clock.tick(FPS)
+
+        score_text = SCORE_FONT.render("Score: {}".format(str(score)), 1, BLACK)
 
         bird.y += bird.falling_speed
 
@@ -119,19 +129,23 @@ def main():
         for pole in poles:
             pole.x -= pole.move_speed
 
-            render(pole)
+            render(pole, score_text)
 
             if has_collided(pole):
                 pygame.time.delay(500)
                 message_box("You lost!", "Play again?")
-                pygame.time.delay(1000)
                 restart_game()
                 break
 
+            if bird.x >= pole.x and score_update:
+                score += 1
+                score_update = False
+
             if pole.x <= - POLE_IMAGE.get_width():
                 poles.pop(0)
+                score_update = True
                 poles.append(Pole(WIDTH + POLE_IMAGE.get_width(), 0, HEIGHT / 2))
-                poles[0].size_between = random.randint(30, 55)
+                poles[0].size_between = random.randint(30, 45)
 
         bird.y = bird.y if bird.y >= 0 else 0
 
